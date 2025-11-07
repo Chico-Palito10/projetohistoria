2b5958d24fba8b5f5005796319ef10385d9c6ce6
Chico-Palito10 <raffael.iema18@gmail.com>
2025-11-07 00:19:48 -0300

    Add files via upload

--- src/config/database.js
+++ src/config/database.js
@@ -26,9 +26,9 @@ const pool = mysql.createPool({
 // Criar banco de dados e tabelas se não existirem
 const setupDatabase = async () => {
   const connection = mysql.createConnection({
-    host: "sql10.freesqldatabase.com",
-    user: "sql10805361",
-    password: "GxS3dYizIu",
+    host: process.env.DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
   });
 
   try {
